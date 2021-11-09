13f9a829cd5a6e2f57ad8a19d6a4195fc1545c57
robertludlow <88632474+robertludlow@users.noreply.github.com>
2021-11-09 15:28:49 -0500

    Update and rename app.txt to 8ball.js

--- 8ball.js
+++ 8ball.js
@@ -3,7 +3,7 @@ const 8ball = (user_question) => {
     console.log(user_question);
 
     const phrases_yes = ["It is certain", "Without a doubt", "You may rely on it", "Yes definitely", "It is decidedly so", "As I see it, yes", "Most likely", "Yes", "Outlook good", "Signs point to yes"]
-    const phrases_no = ["Don’t count on it", "Outlook not so good", "My sources say no", "Very doubtful", "My reply is no"]
+    const phrases_no = ["Donâ€™t count on it", "Outlook not so good", "My sources say no", "Very doubtful", "My reply is no"]
     const phrases_try_again = ["Reply hazy try again", "Better not tell you now", "Ask again later", "Cannot predict now", "Concentrate and ask again"];
         //the magic 8 ball response.
 
@@ -30,9 +30,5 @@ const 8ball = (user_question) => {
             break;
         //selects yes, no, or try by randomly assign a number from 0-2, then selects a random phrase and reply if any by same method.
     }
-
-
-
-
 }
 
